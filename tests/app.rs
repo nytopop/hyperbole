8ff0b9b487de900c6643a1c5aa97a3fb19cb32de
nytopop <ericizoita@gmail.com>
2020-06-14 11:01:44 -0700

    re-implement removed tests as integration tests
    It appears that linking errors are only encountered if the tests are in
    the main crate, which is strange but workable.

---
+++ tests/app.rs
@@ -0,0 +1,98 @@
+use hyper::Body;
+use hyperbole::*;
+
+#[test]
+fn test_modify_uri_parser_after_erasure() {
+    let _ = App::empty()
+        .context()
+        .map(|_: Hlist![Body]| record![])
+        .map(|_: record![]| record![foo = 40u64])
+        .path(path![bar: u32])
+        .map(|_: record![bar, foo]| record![])
+        .collapse();
+
+    let _ = App::new(record![x = 40])
+        .context()
+        .map(|_: record![x]| record![])
+        .path(path![y: u32])
+        .map(|_: record![y]| record![])
+        .collapse();
+
+    let _ = App::empty()
+        .context()
+        .map(|_: record![]| record![x = 40])
+        .map(|cx: record![x]| cx)
+        .path(path![y: u32])
+        .map(|_: record![y]| record![])
+        .collapse();
+}
+
+#[tokio::test]
+async fn test_basic_uri_parsing() {
+    async fn handle(cx: record![x: u32, y: f64]) -> String {
+        let (x, y) = cx.into();
+        format!("x: {}, y: {}", x, y)
+    }
+
+    let app = App::empty()
+        .context_path(path![x: u32 / y: f64])
+        .get(path![], handle)
+        .collapse()
+        .test_client();
+
+    let r = app.get("/44/55").dispatch().await;
+    assert_eq!("x: 44, y: 55", r.body());
+    assert!(r.status().is_success());
+
+    let r = app.get("/more/fdfdf").dispatch().await;
+    assert_eq!(
+        r#"failed to parse "more" in uri: invalid digit found in string"#,
+        r.body()
+    );
+    assert!(r.status().is_client_error());
+
+    let r = app.get("/4848484/fdfdf").dispatch().await;
+    assert_eq!(
+        r#"failed to parse "fdfdf" in uri: invalid float literal"#,
+        r.body()
+    );
+    assert!(r.status().is_client_error());
+}
+
+#[tokio::test]
+async fn test_route_dispatch_with_ctx_val() {
+    async fn handle(cx: record![val: u32]) -> String {
+        format!("{}", *cx.head)
+    }
+
+    let app = App::empty()
+        .context()
+        .map(|_: record![]| record![val = 0])
+        .get(path!["a"], handle)
+        .map(|_: record![val]| record![val = 1])
+        .get(path!["b"], handle)
+        .map(|_: record![val]| record![val = 2])
+        .get(path!["c"], handle)
+        .map(|_: record![val]| record![val = 3])
+        .get(path!["d"], handle)
+        .map(|_: record![val]| record![val = 4])
+        .get(path!["e"], handle)
+        .map(|_: record![val]| record![val = 5])
+        .get(path!["f"], handle)
+        .collapse()
+        .test_client();
+
+    for (path, val) in vec![
+        ("/a", 0u32),
+        ("/b", 1),
+        ("/c", 2),
+        ("/d", 3),
+        ("/e", 4),
+        ("/f", 5),
+    ] {
+        let r = app.get(path).dispatch().await;
+
+        assert_eq!(&format!("{}", val), r.body());
+        assert!(r.status().is_success());
+    }
+}
---
+++ tests/body.rs
@@ -0,0 +1,34 @@
+use hyper::body::to_bytes;
+use hyperbole::{reply::Reply, *};
+
+#[tokio::test]
+async fn test_json_response() {
+    let app = App::empty()
+        .context()
+        .get(path![x: u32 / y: f64], |cx: record![x, y]| async move {
+            reply::jsonr(&cx)
+        })
+        .collapse()
+        .test_client();
+
+    let r = app.get("/44/55").dispatch().await;
+    assert_eq!(r#"{"x":44,"y":55.0}"#, r.body());
+    assert!(r.status().is_success());
+}
+
+#[tokio::test]
+async fn test_jsonr_errors() {
+    let input = r#"{"a":3,"b":32324,"c":345345.34}"#;
+
+    let res = body::jsonr::<record![a: String, b: u32, c: f32]>(hlist![input.into()])
+        .await
+        .unwrap_err();
+
+    let body = res.into_response().into_body();
+    let raw = to_bytes(body).await.unwrap();
+
+    assert_eq!(
+        "failed to parse body: invalid type: integer `3`, expected a string at line 1 column 31",
+        String::from_utf8_lossy(&*raw).into_owned(),
+    );
+}
